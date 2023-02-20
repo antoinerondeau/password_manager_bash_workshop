a39ba31721787bd3a9a9584101f1df2f61c4b307
antoinerondeau <72025063+antoinerondeau@users.noreply.github.com>
2023-02-20 14:16:21 +0000

    feat: step 3

--- password_manager_correct.sh
+++ password_manager_correct.sh
@@ -9,8 +9,20 @@ fi
 # Step 2
 add_password() {
   read -p "Enter the service name : " service
-  read -p "Entrez the password : " password
+  read -p "Enter the password : " password
   encrypted_password=$(echo "$password" | openssl enc -aes-256-cbc -a -pbkdf2)
   echo "$service:$encrypted_password" >> "$file"
   echo "Password succesfully added."
+}
+
+# Step 3
+list_passwords() {
+  read -p "Passwords for the service :" service
+  lines=$(grep "$service" $file)
+  while read line; do
+    username=$(echo "$line" | cut -d ":" -f1)
+    encrypted_password=$(echo "$line" | cut -d ":" -f2)
+    password=$(echo "$encrypted_password" | openssl enc -d -aes-256-cbc -a -pbkdf2)
+    echo "password for $username is : $password"
+  done <<< "$lines"
 }
