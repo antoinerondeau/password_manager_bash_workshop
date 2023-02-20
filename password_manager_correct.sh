09d0bd74e332d8971203630d9ad91cbf84cf95d7
antoinerondeau <72025063+antoinerondeau@users.noreply.github.com>
2023-02-20 13:43:14 +0000

    feat: Step 2

--- password_manager_correct.sh
+++ password_manager_correct.sh
@@ -5,3 +5,12 @@ file="passwords.txt"
 if [ ! -f "$file" ]; then
   touch "$file"
 fi
+
+# Step 2
+add_password() {
+  read -p "Enter the service name : " service
+  read -p "Entrez the password : " password
+  encrypted_password=$(echo "$password" | openssl enc -aes-256-cbc -a -pbkdf2)
+  echo "$service:$encrypted_password" >> "$file"
+  echo "Password succesfully added."
+}
